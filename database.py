9bd79556899587ee30c5bd2303a0c20851e05a02
edwinmerchang-glitch <edwinmerchang@gmail.com>
2026-02-25 17:41:59 -0500

    Add files via upload

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String, Date, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///data.db", connect_args={"check_same_thread": False})
